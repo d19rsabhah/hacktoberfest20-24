e9a09ea60f90d999cc918a821d1369f92368baaf
Rishav Das <rishavdas27602@gmail.com>
2024-10-23 22:01:00 +0530

    Add files via upload

---
+++ Zero_One_KnapSack.java
@@ -0,0 +1,19 @@
+package Data_Structure.Dynamik_Programming;
+
+import java.lang.*;
+import java.util.*;
+import java.util.Arrays;
+public class Zero_One_KnapSack
+{
+	public static void main(String []args)
+	{
+		int w = 10;
+		int Price[] = {20, 30, 10, 50};
+		int Weight[] = {1, 3, 4, 6};
+		int size = Price.length;
+		System.out.println(Z0_KnapSack(w, Price, Weight, size));
+	}
+    static int Z0_KnapSack(int W, int P[], int We[], int n){
+		return 0;
+	}
+}
